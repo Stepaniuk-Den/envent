a1ff33bc9425f16d371c4395aa429c4d813caaf6
StDen <stepaniuk.den@gmail.com>
2024-08-10 10:13:23 +0300

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -4,5 +4,11 @@ export const rubik = Rubik({
   subsets: ["latin"],
   variable: "--font-rubik",
 });
-export const openSans = Open_Sans({ variable: "--font-open-sands" });
-export const montserrat = Montserrat({ variable: "--font-montserrat" });
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sands",
+});
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
