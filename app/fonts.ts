446b0a47c23a038d21fa470a256872186dacaae9
StDen <stepaniuk.den@gmail.com>
2024-08-10 10:11:02 +0300

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import { Rubik, Open_Sans, Montserrat } from "next/font/google";
 
 export const rubik = Rubik({
-  subsets: ["latin", "cyrillic"],
+  subsets: ["latin"],
   variable: "--font-rubik",
 });
 export const openSans = Open_Sans({ variable: "--font-open-sands" });
