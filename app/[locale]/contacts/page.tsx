debd068d9045fade25e40354513672af9c60caa2
StDen <stepaniuk.den@gmail.com>
2024-08-30 13:15:13 +0300

    create conatactsPage

--- app/[locale]/contacts/page.tsx
+++ app/[locale]/contacts/page.tsx
@@ -1,3 +1,13 @@
-export default function Contacts() {
-  return <div>Contacts</div>;
-}
+import { unstable_setRequestLocale } from "next-intl/server";
+import styles from "./contacts.module.scss";
+
+type Props = {
+  params: { locale: string };
+};
+
+const Contacts: React.FC<Props> = async ({ params: { locale } }) => {
+  unstable_setRequestLocale(locale);
+  return <div></div>;
+};
+
+export default Contacts;
