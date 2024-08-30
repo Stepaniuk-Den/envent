9017f418d4005120ebf7540f5a8b569d1c7869b4
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-08-30 12:25:36 +0300

    Add ImgCarousel to ServiceItem,change store for ImgCarousel

--- app/[locale]/components/ServiceItemDescription/index.tsx
+++ app/[locale]/components/ServiceItemDescription/index.tsx
@@ -1,21 +1,26 @@
-import styles from "./serviceItemDescription.module.scss";
 import { PropsServiceCard } from "@/helpers/interfaces";
-
+import styles from "./serviceItemDescription.module.scss";
 
 const ServiceItemDescription = ({ t }: { t: PropsServiceCard }) => {
-  const {
-    p1,
-    p2,
-    p3,
-    p4,
-  } = t;
+  const { p1, p2, p3, p4 } = t;
 
   return (
     <section className={`${styles.service_item} container`}>
-        <div className={styles.service_container}>
-            <div className={styles.left_wrapper}><p className={styles.text}>{p1}</p><p className={styles.text}>{p2}</p></div>
-            <div className={styles.right_wrapper}><p className={styles.text}>{p3}</p><p className={styles.text}>{p4}</p></div>
-        </div>
+      <div className={styles.service_container}>
+        <p className={styles.text}>{p1}</p>
+        <p
+          className={styles.text}
+          dangerouslySetInnerHTML={{ __html: p4 || "" }}
+        ></p>
+        <p
+          className={styles.text}
+          dangerouslySetInnerHTML={{ __html: p2 || "" }}
+        ></p>
+        <p
+          className={styles.text}
+          dangerouslySetInnerHTML={{ __html: p3 || "" }}
+        ></p>
+      </div>
     </section>
   );
 };
