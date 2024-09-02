8ca37fe9fac755c9f8a8482156fb52f53e8ec12b
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-09-02 12:51:44 +0300

    Update carousel-service-store.ts

--- stores/carousel-service-store.ts
+++ stores/carousel-service-store.ts
@@ -1,19 +1,3 @@
-// import { create } from "zustand";
-
-// interface CarouselItem {
-//   id: number;
-// }
-
-// interface CarouselServiceStore {
-//   keyImagesService: CarouselItem;
-//   update: (keyImagesService: CarouselItem) => void;
-// }
-
-// export const useCarouselServiceStore = create<CarouselServiceStore>((set) => ({
-//   keyImagesService: { id: 1 },
-//   update: (keyImagesService) => set(() => ({ keyImagesService })),
-// }));
-
 import { create } from "zustand";
 
 interface CarouselItem {
@@ -26,9 +10,25 @@ interface CarouselServiceStore {
 }
 
 export const useCarouselServiceStore = create<CarouselServiceStore>((set) => ({
-  keyImagesService: { id: Number(localStorage.getItem("carouselServiceId")) || 1 },
-  update: (keyImagesService: CarouselItem) => {
-    localStorage.setItem("carouselServiceId", String(keyImagesService.id));
-    set(() => ({ keyImagesService }));
-  },
+  keyImagesService: { id: 1 },
+  update: (keyImagesService) => set(() => ({ keyImagesService })),
 }));
+
+// import { create } from "zustand";
+
+// interface CarouselItem {
+//   id: number;
+// }
+
+// interface CarouselServiceStore {
+//   keyImagesService: CarouselItem;
+//   update: (keyImagesService: CarouselItem) => void;
+// }
+
+// export const useCarouselServiceStore = create<CarouselServiceStore>((set) => ({
+//   keyImagesService: { id: Number(localStorage.getItem("carouselServiceId")) || 1 },
+//   update: (keyImagesService: CarouselItem) => {
+//     localStorage.setItem("carouselServiceId", String(keyImagesService.id));
+//     set(() => ({ keyImagesService }));
+//   },
+// }));
