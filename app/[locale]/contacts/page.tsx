327ba26b97b79e834181850a56dca0e6007b4d15
StDen <stepaniuk.den@gmail.com>
2024-09-24 11:25:15 +0300

    add sharp

--- app/[locale]/contacts/page.tsx
+++ app/[locale]/contacts/page.tsx
@@ -25,6 +25,10 @@ const Contacts: React.FC<Props> = async ({ params: { locale } }) => {
       <Hero imageSrc={HeroAboutBG} t={contactsT.hero} className="contacts">
         <HeroRightSideList className="contacts" t={heroRightSideT.contact} />
       </Hero>
+      <HeroRightSideList
+        className="contacts_mobile"
+        t={heroRightSideT.contact}
+      />
       <div className={styles.contacts}>
         <QuestionsSection params={{ locale }} type="form" />
         <MapSection locationBtn={contactsT.locationBtn.description} />
