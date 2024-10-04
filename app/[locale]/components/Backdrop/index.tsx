cdb95695b5e7ec4aa3bcec947d208c58bd29d3bb
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-10-04 09:03:09 +0200

    add imports images, update projectSection, categorySlug, add  CaregoryProjectsList, separated backdrop, add createPortal, fix styles

---
+++ app/[locale]/components/Backdrop/index.tsx
@@ -0,0 +1,215 @@
+import styles from "./backdrop.module.scss";
+import { AnimatePresence, motion, wrap } from "framer-motion";
+import ArrowLeft from "@/public/icons/arrow-left.svg";
+import ArrowRight from "@/public/icons/arrow-right.svg";
+import Close from "@/public/icons/close.svg";
+import { useModal } from "@/helpers/useModal";
+import Image from "next/image";
+import BackdropButton from "../Buttons/BackdropButton";
+import ReactPortal from "@/helpers/createPortal";
+import { useEffect, useState } from "react";
+import { IPropsProjectItem } from "@/helpers/interfaces";
+import { handleCloseBackdrop } from "@/helpers/handleOpenAndCloseBackdrop";
+import { handleNext, handlePrev } from "@/helpers/useClickPrevAndNext";
+// import { wrap } from "popmotion";
+
+interface IBackdropProps {
+  //   children: React.ReactElement;
+  //   imgList: IPropsProjectItem[];
+  //   imgAlt: string[];
+  imgList: string[];
+  alt: string;
+  currentIndex: number | null;
+  setCurrentIndex: (currentIndex: number) => void;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  //   src: string;
+  //   direction: any;
+  //   page: number;
+  //   paginate: boolean;
+  //   setPage: (page: number[]) => void;
+}
+
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
+
+const Backdrop = ({
+  imgList,
+  currentIndex,
+  setCurrentIndex,
+  isOpen,
+  setIsOpen,
+  //   src,
+  //   imgAlt,
+  alt,
+}: //   direction,
+//   page,
+//   setPage,
+IBackdropProps) => {
+  const [[page, direction], setPage] = useState<number[]>([
+    currentIndex ?? 0,
+    0,
+  ]);
+
+  useEffect(() => {
+    setPage([currentIndex ?? 0, 0]);
+  }, [currentIndex]);
+
+  //   const imageIndex = wrap(currentIndex || 0, imgList.length, page);
+  const imageIndex = (page + imgList.length) % imgList.length;
+  //   const imageAlt = wrap(currentIndex || 0, imgAlt.length, page);
+
+  const paginate = (newDirection: number) => {
+    // setPage([page + newDirection, newDirection]);
+    const newIndex = (page + newDirection + imgList.length) % imgList.length;
+    setPage([newIndex, newDirection]);
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <ReactPortal wrapperId="react-portal-modal-container">
+      <>
+        <AnimatePresence mode="wait" initial={false} custom={direction}>
+          {isOpen && currentIndex !== null && (
+            <motion.div
+              className={`${styles.backdrop} overlay`}
+              //   initial={{ opacity: 0 }}
+              //   animate={{ opacity: 1 }}
+              //   exit={{ opacity: 0 }}
+              //   ====
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              transition={{ delay: isOpen ? 0.2 : 0, duration: 0.3 }}
+              onAnimationComplete={() => {
+                if (!isOpen) handleCloseBackdrop(setIsOpen);
+              }}
+              onClick={() => handleCloseBackdrop(setIsOpen)}
+            >
+              <AnimatePresence initial={false} custom={direction}>
+                <motion.div
+                  key={page}
+                  custom={direction}
+                  variants={variants}
+                  className={styles.modalContent}
+                  //   initial={{ scale: 0.8, opacity: 0 }}
+                  //   animate={{ scale: 1, opacity: 1 }}
+                  //   exit={{ scale: 0.8, opacity: 0 }}
+                  //   transition={{ delay: isOpen ? 0.2 : 0, duration: 0.3 }}
+                  //   onAnimationComplete={() => {
+                  //     if (!isOpen) handleCloseBackdrop(setIsOpen);
+                  //   }}
+                  //   onClick={(e) => e.stopPropagation()}
+                  //   ==============================
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { type: "spring", stiffness: 300, damping: 30 },
+                    opacity: { duration: 0.2 },
+                  }}
+                  drag="x"
+                  dragConstraints={{ left: 0, right: 0 }}
+                  dragElastic={1}
+                  onDragEnd={(e, { offset, velocity }) => {
+                    const swipe = swipePower(offset.x, velocity.x);
+
+                    if (swipe < -swipeConfidenceThreshold) {
+                      paginate(1);
+                    } else if (swipe > swipeConfidenceThreshold) {
+                      paginate(-1);
+                    }
+                  }}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <Image
+                    className={styles.images}
+                    src={imgList[imageIndex]}
+                    // alt={imgAlt[imageAlt]}
+                    alt={alt}
+                    fill={true}
+                    sizes="(max-width: 767.98px) 460px, (max-width: 1023.98px) 660px, 800px"
+                  />
+                </motion.div>
+              </AnimatePresence>
+
+              <p className={styles.counter}>
+                {imageIndex + 1} of {imgList.length}
+              </p>
+              <BackdropButton
+                className={styles.closeButton}
+                onClick={() => handleCloseBackdrop(setIsOpen)}
+                type="button"
+                aria-label="Close"
+              >
+                <Close className={styles.closeSVG} />
+              </BackdropButton>
+
+              <BackdropButton
+                className={styles.prevButton}
+                // onClick={() =>
+                //   handlePrev({
+                //     currentIndex,
+                //     setCurrentIndex,
+                //     array: imgList,
+                //   })
+                // }
+                onClick={(e: any) => {
+                  e.stopPropagation();
+                  paginate(-1);
+                }}
+                type="button"
+                aria-label="Swipe to previous"
+              >
+                <ArrowLeft className={styles.ArrowLeftSVG} />
+              </BackdropButton>
+              <BackdropButton
+                className={styles.nextButton}
+                // onClick={() =>
+                //   handleNext({
+                //     currentIndex,
+                //     setCurrentIndex,
+                //     array: imgList,
+                //   })
+                // }
+                onClick={(e: any) => {
+                  e.stopPropagation();
+                  paginate(1);
+                }}
+                type="button"
+                aria-label="Swipe to next"
+              >
+                <ArrowRight className={styles.ArrowRightSVG} />
+              </BackdropButton>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </>
+    </ReactPortal>
+  );
+};
+
+export default Backdrop;
--- app/[locale]/components/ProjectsSection/index.tsx
+++ app/[locale]/components/ProjectsSection/index.tsx
@@ -1,23 +1,18 @@
-// "use client";
+"use client";
 
 import styles from "./projectSection.module.scss";
-// import { AnimatePresence, motion } from "framer-motion";
-// import Image from "next/image";
 import MainButton from "../Buttons/MainButton";
-// import { useEffect, useState } from "react";
-// import ArrowLeft from "@/public/icons/arrow-left.svg";
-// import ArrowRight from "@/public/icons/arrow-right.svg";
-// import Close from "@/public/icons/close.svg";
-// import BackdropButton from "../Buttons/BackdropButton";
+import { useState } from "react";
 import { Link } from "@/navigation";
-// import { useRouter } from "next/navigation";
-// import { handleNext, handlePrev } from "@/helpers/useClickPrevAndNext";
 import AnimatedTitle from "../AnimatedTitle";
 import { MainPageT } from "@/messages/types/MainPageT";
 import ProjectItem from "../ProjectItem";
 import { ProjectsPageT } from "@/messages/types/ProjectsPageT";
 import Line from "../Line";
 import { ProjectItems } from "@/data/ProjectItems";
+import { useModal } from "@/helpers/useModal";
+import { handleOpenBackdrop } from "@/helpers/handleOpenAndCloseBackdrop";
+import Backdrop from "../Backdrop";
 
 // interface IPropsProjectList {
 //   projectList: IPropsProjectItem[];
@@ -31,55 +26,36 @@ const ProjectSection: React.FC<{
   t2: ProjectsPageT["projects"];
   // t2: ProjectWithBase64[];
 }> = ({ t, t2 }) => {
-  //= ({ t }: IProjectsProps) => {
-  // =======================================
-  // const [isOpen, setIsOpen] = useState(false);
-  // const [currentIndex, setCurrentIndex] = useState<number>(0);
-  // =======================================
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   const projectsList = t2 ? Object.values(t2) : [];
   // const projectsList = t2 ? t2 : [];
   // const projectsList = t2 || []; ------
   const recentProjectsList = projectsList
-    // const [currentIndex, setCurrentIndex] = useState<number | null>(null);
-    // const [isHovered, setHovered] = useState(false);
-    // const router = useRouter();
-
     .sort((a, b) => Number(b.id) - Number(a.id))
     .slice(0, 6);
 
+  const recentProjectsListMainImgs = recentProjectsList
+    .map((project) => project.mainImg)
+    .filter((img): img is string => !!img);
+
+  // const recentImgAlt = recentProjectsList
+  //   .map((project) => project.mainAlt)
+  //   .filter((alt) => alt);
+
+  useModal(isOpen, setIsOpen);
+
   // ===================================================
-  // useEffect(() => {
-  //   if (isOpen) {
-  //     // document.body.style.overflow = "hidden";
-  //     document.documentElement.style.overflow = "hidden";
-  //   } else {
-  //     // document.body.style.overflow = "auto";
-  //     document.documentElement.style.overflow = "auto";
-  //   }
-
-  //   const handleKeyDown = (e: KeyboardEvent) => {
-  //     if (e.code === "Escape") {
-  //       setIsOpen(false);
-  //     }
-  //   };
-  //   window.addEventListener("keydown", handleKeyDown);
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyDown);
-  //     document.body.style.overflow = "auto";
-  //     document.documentElement.style.overflow = "auto";
-  //   };
-  // }, [isOpen]);
-
   // const handleOpenBackdrop = (index: number) => {
   //   setCurrentIndex(index);
   //   setIsOpen(true);
   // };
   // const handleCloseBackdrop = () => {
   //   setIsOpen(false);
-  // setCurrentIndex(null);
+  //   // setCurrentIndex(null);
   // };
-  // =================================================================
+  // =====================================================
 
   // const handleNext = () => {
   //   if (currentIndex !== null) {
@@ -98,7 +74,6 @@ const ProjectSection: React.FC<{
     <section className={styles.projectSection}>
       <div className={`${styles.projectContainer} container`}>
         <AnimatedTitle title={t.title} className={styles.sectionTitle} />
-        {/* ------------------------------------ */}
         <Line className="yellow-center" />
         <ul className={styles.projectList}>
           {/* {projectsList.map((project: IPropsProjectItem, index) => ( */}
@@ -109,10 +84,9 @@ const ProjectSection: React.FC<{
             //   (project) => project.title === projectItem.src
             // );
             const imgName = projectItem.mainImg.split("/").pop()?.split(".")[0];
-            const currentImg = [
-              ...ProjectItems.trade_complexes,
-              ...ProjectItems.hskc,
-            ].find((item) => item.name === imgName);
+            const currentImg = [...ProjectItems].find(
+              (item) => item.name === imgName
+            );
             if (!currentImg) return;
             return (
               <ProjectItem
@@ -121,105 +95,112 @@ const ProjectSection: React.FC<{
                 t={projectItem}
                 currentImg={currentImg.srcImg}
                 // base64={projectItem.base64}
-                // onClick={() => handleOpenBackdrop(index)}
+                onClick={() =>
+                  handleOpenBackdrop(index, setCurrentIndex, setIsOpen)
+                }
               />
-
               // ))}
             );
           })}
         </ul>
-        {/* <Link className={styles.viewAllBtnLink} href="/projects"> */}
-        <MainButton
-          type="button"
-          color="black"
-          // onClick={() => router.push("/projects")}
-          className="btnProjectsLink"
-        >
+        <MainButton type="button" color="black" className="btnProjectsLink">
           <Link className={styles.viewAllBtnLink} href="/projects">
             {t.viewAll}
           </Link>
         </MainButton>
-        {/* </Link> */}
 
-        {/* ==== BACKDROP ==== */}
-        {/* <AnimatePresence mode="wait">
-          {isOpen && currentIndex !== null && (
-            <motion.div
-              className={styles.backdrop}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              onClick={handleCloseBackdrop}
-            >
-              <motion.div
-                className={styles.modalContent}
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.8, opacity: 0 }}
-                transition={{ delay: isOpen ? 0.2 : 0, duration: 0.3 }}
-                onAnimationComplete={() => {
-                  if (!isOpen) handleCloseBackdrop();
-                }}
-                onClick={(e) => e.stopPropagation()}
-              >
-                <BackdropButton
-                  className={styles.closeButton}
-                  onClick={handleCloseBackdrop}
-                  type="button"
-                  aria-label="Close"
-                >
-                  <Close className={styles.closeSVG} />
-                </BackdropButton>
-                <BackdropButton
-                  className={styles.prevButton}
-                  // onClick={handlePrev}
-                  onClick={() =>
-                    handlePrev({
-                      currentIndex,
-                      setCurrentIndex,
-                      array: recentProjectsList,
-                    })
-                  }
-                  type="button"
-                  aria-label="Swipe to previous"
-                >
-                  <ArrowLeft className={styles.ArrowLeftSVG} />
-                </BackdropButton>
-
-                <Image
-                  src={recentProjectsList[currentIndex].mainImg}
-                  alt={recentProjectsList[currentIndex].mainAlt}
-                  fill={true}
-                  sizes="(max-width: 767.98px) 460px, (max-width: 1023.98px) 660px, 800px"
-                />
-
-                <BackdropButton
-                  className={styles.nextButton}
-                  // onClick={handleNext}
-                  onClick={() =>
-                    handleNext({
-                      currentIndex,
-                      setCurrentIndex,
-                      array: recentProjectsList,
-                    })
-                  }
-                  type="button"
-                  aria-label="Swipe to next"
-                >
-                  <ArrowRight className={styles.ArrowRightSVG} />
-                </BackdropButton>
-                <p className={styles.counter}>
-                  {currentIndex + 1} of {recentProjectsList.length}
-                </p>
-              </motion.div>
-            </motion.div>
-          )}
-        </AnimatePresence> */}
+        {/* ========BACKDROP======== */}
+        <Backdrop
+          imgList={recentProjectsListMainImgs}
+          // imgAlt={recentImgAlt}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          alt="..."
+        />
       </div>
     </section>
   );
 };
 export default ProjectSection;
+
+// ================
+// ==== BACKDROP ====
+//  <AnimatePresence mode="wait">
+//     {isOpen && currentIndex !== null && (
+//       <motion.div
+//         className={`${styles.backdrop} overlay`}
+//         initial={{ opacity: 0 }}
+//         animate={{ opacity: 1 }}
+//         exit={{ opacity: 0 }}
+//         onClick={() => handleCloseBackdrop(setIsOpen)}
+//       >
+//         <motion.div
+//           className={styles.modalContent}
+//           initial={{ scale: 0.8, opacity: 0 }}
+//           animate={{ scale: 1, opacity: 1 }}
+//           exit={{ scale: 0.8, opacity: 0 }}
+//           transition={{ delay: isOpen ? 0.2 : 0, duration: 0.3 }}
+//           onAnimationComplete={() => {
+//             if (!isOpen) handleCloseBackdrop(setIsOpen);
+//           }}
+//           onClick={(e) => e.stopPropagation()}
+//         >
+//           <BackdropButton
+//             className={styles.closeButton}
+//             onClick={() => handleCloseBackdrop(setIsOpen)}
+//             type="button"
+//             aria-label="Close"
+//           >
+//             <Close className={styles.closeSVG} />
+//           </BackdropButton>
+//           <BackdropButton
+//             className={styles.prevButton}
+//             // onClick={handlePrev}
+//             onClick={() =>
+//               handlePrev({
+//                 currentIndex,
+//                 setCurrentIndex,
+//                 array: recentProjectsList,
+//               })
+//             }
+//             type="button"
+//             aria-label="Swipe to previous"
+//           >
+//             <ArrowLeft className={styles.ArrowLeftSVG} />
+//           </BackdropButton>
+
+//           <Image
+//             className={styles.images}
+//             src={recentProjectsList[currentIndex].mainImg}
+//             alt={recentProjectsList[currentIndex].mainAlt}
+//             fill={true}
+//             sizes="(max-width: 767.98px) 460px, (max-width: 1023.98px) 660px, 800px"
+//           />
+
+//           <BackdropButton
+//             className={styles.nextButton}
+//             // onClick={handleNext}
+//             onClick={() =>
+//               handleNext({
+//                 currentIndex,
+//                 setCurrentIndex,
+//                 array: recentProjectsList,
+//               })
+//             }
+//             type="button"
+//             aria-label="Swipe to next"
+//           >
+//             <ArrowRight className={styles.ArrowRightSVG} />
+//           </BackdropButton>
+//           <p className={styles.counter}>
+//             {currentIndex + 1} of {recentProjectsList.length}
+//           </p>
+//         </motion.div>
+//       </motion.div>
+//     )}
+//   </AnimatePresence>
 
 // ================================================================
 
